98574f862d3fe76497c6da53becf776ec0f2468d
Merry Caroline Purser <purser.merry@gmail.com>
2019-10-18 13:55:06 -0600

    Add files via upload

---
+++ Issue2/draft_terminal.py
@@ -0,0 +1,9 @@
+import os
+
+f= open("out_file.txt", "w+")
+
+
+cmd = 'python print_play.py'
+
+os.system(cmd)
+
---
+++ Issue2/model.py
@@ -0,0 +1,10 @@
+import time
+
+
+print("Python Python is Fun!")
+print("Timestamp: %s" % time.ctime())
+
+time.sleep(3)
+
+print("Python message 2!")
+print("Timestamp: %s" % time.ctime())
